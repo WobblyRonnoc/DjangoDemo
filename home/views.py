bffdd0a6a85a56c938ac6d2d6f3b18fe06042e55
Connor <WobblyProductions@hotmail.com>
2023-07-26 15:25:57 -0400

    stormy saving for saftey

--- home/views.py
+++ home/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(response):
-    return render(response, 'index.html', {"index": "index"})
+    return render(response, 'index.html', {
+        "index": "index",
+        "header_message": "This page is actually 127.0.0.1:8000/home/index.html"})
